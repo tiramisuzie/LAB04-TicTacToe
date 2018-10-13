dd2530cbba215c2f4a75edb30174b7debade4d5b
suzanne su <suhysu@gmail.com>
2018-10-13 13:53:37 -0700

    initial commmit

--- LAB04-TicTacToe/Program.cs
+++ LAB04-TicTacToe/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace LAB04_TicTacToe
+namespace Lab04_TicTacToe
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		static void PlayGame()
+		{
+			// TODO: Instantiate your players
+			// Create the Game
+			// Play the Game
+			// Output the winner
+		}
+	}
 }
